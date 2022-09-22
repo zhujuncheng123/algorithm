f6122e1b165ff6568526a99d0ecdec70cac9582c
zhujc <279661265@qq.com>
2022-09-22 10:41:32 +0800

    first 提交

---
+++ Array/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * @author: zhujc
+ * @Title: Hello
+ * @ProjectName: algorithm
+ * @Description:
+ * @date: 2022/9/22 10:40
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("初次提交");
+    }
+}
